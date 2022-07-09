7d74d5e96595027afdd6ce0a60f97c964f3b16d9
atuldiwedi <atuldiwedi78@gmail.com>
2022-07-09 11:30:19 +0530

    message

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import './screens/tabs_screen.dart';
+import './screens/meal_detail_screen.dart';
+import 'screens/category_meals_screen.dart';
+import 'screens/Categories_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Foodze',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 225, 1),
+        fontFamily: GoogleFonts.raleway().fontFamily,
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              subtitle1: TextStyle(
+                fontSize: 20,
+                fontFamily: GoogleFonts.getFont('Lato').fontFamily,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+      ),
+      routes: {
+        '/': (ctx) => TabsScreen(),
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      // onGenerateRoute: (settings) {
+      //   print(settings.arguments);
+
+      //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Foodze"),
+      ),
+      body: Center(child: Text("Navigate")),
+    );
+  }
+}
---
+++ lib/screens/Categories_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../widgets/Category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map((catData) =>
+              CategoryItem(catData.id, catData.title, catData.color))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20),
+    );
+  }
+}
